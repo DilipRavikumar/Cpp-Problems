eebb5f3381afffc1194bfe18a751dda908e04eeb
Dilip R <128499989+DilipRavikumar@users.noreply.github.com>
2024-10-12 13:36:24 +0530

    Time: 250 ms (86.71%), Space: 102.6 MB (63.29%) - LeetHub

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+                vector<pair<int, int>> events;
+
+        for (auto& interval : intervals) {
+            events.push_back({interval[0], 1});     
+            events.push_back({interval[1] + 1, -1}); 
+        }
+
+        sort(events.begin(), events.end());
+
+        int maxGroups = 0, activeIntervals = 0;
+
+        for (auto& event : events) {
+            activeIntervals += event.second; 
+            maxGroups = max(maxGroups, activeIntervals); 
+        }
+
+        return maxGroups;
+    }
+};
+
+ 
